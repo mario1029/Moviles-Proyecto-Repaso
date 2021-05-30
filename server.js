b1f0c34ffb995a824e6a4f54bc77fc5db75c9c63
mario1029 <magr279@gmail.com>
2021-05-29 20:44:40 -0400

    Borrar archivos repetidos...
    Tenia repetidos dos archivos ...

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index'); 
-
-console.log("jelo2")
-
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () =>{
-	 console.log('Listening on port',port);
-	});
