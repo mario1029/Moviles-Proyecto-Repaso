38dfb3ccd344a5421e7da108a4a8f44756f91503
mario1029 <magr279@gmail.com>
2021-06-03 09:16:03 -0400

    delete

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,13 +6,12 @@ const { updateUserFieldsValidation, checkResult } = require('../validations/fiel
 const {deleteUser, updateUser} = require('../helpers/users');
 
 router.get('/:id', isAuth, async (req, res)=>{
-	console.log(req.params.id)
 	const data = await getUserByID(req.params.id);
 	res.send({ci: data.cedula, nombre: data.nombre});
 })
 
 router.put('/:id', isAuth,  updateUserFieldsValidation, checkResult, async (req, res) => {
-  const { cedula } = req.params.id;
+  const cedula = req.params.id;
   try {
     const data = await updateUser( req.body, cedula );
     res.status(200).json({ status: 200, usuario: data, message: 'Usuario actualizado!' });
@@ -20,5 +19,15 @@ router.put('/:id', isAuth,  updateUserFieldsValidation, checkResult, async (req,
     res.status(500).json({ status: 500, error: e, message: 'Error al actualizar un usuario' });
   }
 });
+
+router.delete('/:id', isAuth, async (req, res)=>{
+  const cedula = req.params.id;
+  try {
+    const data = await deleteUser( cedula );
+    res.status(200).json({ status: 200, message: 'Usuario eliminado!' });
+  } catch (e) {
+    res.status(500).json({ status: 500, error: e, message: 'Error al eliminar usuario' });
+  }
+})
 
 module.exports = router;
